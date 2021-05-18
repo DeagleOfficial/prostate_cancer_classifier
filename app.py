8a1d51be325cec5a082c03e4b971c6ed4db96c1a
Supratik Chakraborty <supratikofficial1@gmail.com>
2021-05-18 12:54:13 +0530

    Made changes

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# import pickle
-# file = open('knnPickle_file', 'rb')
-# model = pickle.load(file)
-# result = model.predict([[13,	19,	135, 1297,	0.141,	0.133,	0.181,	0.059]])
-# print(result)
-
 from flask import Flask, render_template,request
 import pickle
 import numpy as np
-import os
 app = Flask(__name__)
-model = pickle.load(open('knnPickle_file', 'rb'))
+model = pickle.load(open('knnpickle_file', 'rb'))
 
 @app.route('/')
 def home():
@@ -33,5 +26,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
